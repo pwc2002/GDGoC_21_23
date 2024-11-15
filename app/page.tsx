4a3cccef04a05ad0d21e0826cfa27f3dadeb691a
Seungyeop Baek <yup6786@naver.com>
2024-11-15 09:51:19 +0900

    add Notices and .json files

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,68 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import MajorNotices from './data/MajorNotices.json';
+import ScholarshipNotices from './data/ScholarshipNotices.json';
 
 export default function Home() {
+  return(
+    ScholarshipNotice()
+  );
+}
+
+export function ScholarshipNotice() {
+  const { scholarships } = ScholarshipNotices;
+
   return (
     <>
       <div className="absolute top-12 left-5 object-cover h-5 w-5">
-        <img src="./pngwing.com.png" alt="menu button"/>
+        <img src="./pngwing.com.png" alt="menu button" />
       </div>
-      <div className="flex flex-col w-full h-screen absolute top-20 left-10">
-        <div className="text-left text-2xl font-bold relative top-10">장학금 안내</div>
+      <div className="flex flex-col w-full max-w-[90%] h-screen absolute top-20 left-10 px-4">
+        <div className="text-left text-2xl font-bold relative top-10">
+          학부 공지사항
+        </div>
         <div className="relative top-10 text-xl text-gray-600/75 font-semibold">
-          <div className="relative top-5 pt-3 pb-4">
-            <input type="checkbox"></input>
-            <span className="relative left-5">내용</span>
-          </div>
-          <div className="relative top-5 pb-4">
-            <input type="checkbox"></input>
-            <span className="relative left-5">알아서</span>
-          </div>
-          <div className="relative top-5 pb-4">
-            <input type="checkbox"></input>
-            <span className="relative left-5">알잘딱깔센으로</span>
-          </div>
-          <div className="relative top-5 pb-4">
-            <input type="checkbox"></input>
-            <span className="relative left-5">채워주시리라고</span>
-          </div>
-          <div className="relative top-5 pb-4">
-            <input type="checkbox"></input>
-            <span className="relative left-5">생각하셨겠지만 오산입니다. (경기도 오산 아님.)</span>
-          </div>
-          <div className="relative top-10">
-            <span className="relative left-5">머야 이거 주석 어케 담?</span>
-            <br></br>
-            <span className="relative left-5">
-              그치만 이렇게 하면 안되겠죠...
-            </span>
-          </div>
+          {scholarships.map((notice) => (
+            <div key={notice.id} className="relative top-5 pt-3 pb-4 flex items-start gap-5">
+              <input 
+                type="checkbox" 
+                className="flex-shrink-0 mt-1.5"
+                id={`notice-${notice.id}`}
+              />
+              <span className="break-keep">
+                {notice.title}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function MajorNotice() {
+  const { notices } = MajorNotices;
+
+  return (
+    <>
+      <div className="absolute top-12 left-5 object-cover h-5 w-5">
+        <img src="./pngwing.com.png" alt="menu button" />
+      </div>
+      <div className="flex flex-col w-full max-w-[90%] h-screen absolute top-20 left-10 px-4">
+        <div className="text-left text-2xl font-bold relative top-10">
+          학부 공지사항
+        </div>
+        <div className="relative top-10 text-xl text-gray-600/75 font-semibold">
+          {notices.map((notice) => (
+            <div key={notice.id} className="relative top-5 pt-3 pb-4 flex items-start gap-5">
+              <input 
+                type="checkbox" 
+                className="flex-shrink-0 mt-1.5"
+                id={`notice-${notice.id}`}
+              />
+              <span className="break-keep">
+                {notice.title}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </>
