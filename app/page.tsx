8a22be94045d050e017c0d0fc708c04a867e252d
Seungyeop Baek <74288031+bksgyp@users.noreply.github.com>
2024-11-15 10:00:27 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
 import MajorNotices from './data/MajorNotices.json';
 import ScholarshipNotices from './data/ScholarshipNotices.json';
 
