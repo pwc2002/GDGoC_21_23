06eb826d0304b8501a987d182dd14d97f2170501
pwc2002 <pwc2002@naver.com>
2024-11-19 01:44:32 +0900

    ✅[Fix] :  캘린더 모달 수정

--- app/add/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AddPage() {
-  return (
-    <div className="flex flex-col w-full justify-center items-center text-5xl font-bold">
-      일정 추가 페이지
-    </div>
-  );
-}
